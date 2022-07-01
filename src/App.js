7fa3b2ef99117b0eeba203bdac8cbef8fbe3bb84
Keerthana Nuti <keerthana.nuti11@gmail.com>
2022-07-01 20:34:08 +0530

    Added Header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react'
+import HeaderComponent from './components/HeaderComponent';
 import ListExpenseComponent from './components/ListExpenseComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListExpenseComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListExpenseComponent />
+      </div>
+      <FooterComponent />
     </div>
   );
 }
